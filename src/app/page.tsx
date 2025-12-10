ef640db010bfdc96ff00e41807edc550253d3382
Astrogusss <gugusenador@gmail.com>
2025-12-10 14:50:06 -0300

    colocando para limpar os cookies de comeco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import Link from "next/link";
-import style from './page.module.css'
+import style from "./page.module.css";
+import { SessionOff } from "./auth";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const fazerLogout = async () => {
+    await SessionOff();
+    router.push("/login");
+  };
+
   return (
     <div className={style.main}>
-         <Link href={"/login"}>Começar experiência musical!</Link>
+      <button onClick={fazerLogout}>
+        <Link href={"/login"}>Começar experiência musical!</Link>
+      </button>
     </div>
   );
 }
